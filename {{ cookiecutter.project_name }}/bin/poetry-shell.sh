5086b117c00d7a592ae48be4ef8ab7fe1d299c78
Thanate boat Dhirasakdanon <dthanate@gmail.com>
2023-08-07 09:40:44 +0700

    ...

--- {{ cookiecutter.project_name }}/bin/poetry-shell.sh
+++ {{ cookiecutter.project_name }}/bin/poetry-shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $DIR/..
-source .envrc
+source .env
 poetry install --sync --compile
 echo `pwd` > .venv/.project
 exec poetry shell
--- {{ cookiecutter.project_name }}/{{ cookiecutter.package_name }}/lib/helper.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.package_name }}/lib/helper.py
@@ -1,11 +1,10 @@
+import os
 from pathlib import Path
 from typing import Final
 
-import os
 
-
-PROJECT_HOME:Final[str] = os.getenv("PROJECT_HOME", os.path.join(os.path.dirname(__file__), ".."))
-PACKAGE_NAME:Final[str] = os.getenv("PACKAGE_NAME", "{{ cookiecutter.package_name }}")
+PROJECT_HOME: Final[str] = os.getenv("PROJECT_HOME", os.path.join(os.path.dirname(__file__), "../.."))
+PACKAGE_NAME: Final[str] = os.getenv("PACKAGE_NAME", "{{ cookiecutter.package_name }}")
 
 
 def get_project_home() -> Path:
