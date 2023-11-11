0468fe7304497b63fbea455279840bd66b805985
Thanate boat Dhirasakdanon <dthanate@gmail.com>
2023-11-12 00:39:11 +0700

    ...

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/lib/path_helper.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/lib/path_helper.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import Final
 
 
-PROJECT_HOME: Final[str] = os.getenv("PROJECT_HOME", os.path.join(os.path.dirname(__file__), ".."))
+PROJECT_HOME: Final[str] = os.getenv("PROJECT_HOME", os.path.join(os.path.dirname(__file__), "../.."))
 PACKAGE_NAME: Final[str] = os.getenv("PACKAGE_NAME", "{{ cookiecutter.package_name }}")
 
 
